af10112071d4447759a6af5227da81caac63cbe6
XERXES <eng.asghari75@gmail.com>
2023-10-08 18:14:06 +0330

    first push

---
+++ main.js
@@ -0,0 +1,24 @@
+
+// DOM NODES
+let boxes = document.querySelectorAll(".board__item");
+let score = document.querySelector(".scoreboard__score__number");
+
+
+
+// FUNCTIONS
+
+function keyCheck(evt){
+
+}
+
+
+
+
+
+
+
+
+
+
+// EVENTS
+window.addEventListener("keypress", keyCheck)
